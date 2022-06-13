a7f6bc7b8a4cfb415948b4565436727aeec5fc39
Adalberto Ferreira <adalbertofj@gmail.com>
2022-06-13 19:10:43 -0300

    Seller details and eager loading

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -7,11 +7,11 @@ namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
-       
+
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
 
-        
+
 
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
@@ -35,13 +35,13 @@ namespace SalesWebMVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller) 
+        public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Delete(int? id) 
+        public IActionResult Delete(int? id)
         {
             if (id == null)
             {
@@ -58,10 +58,28 @@ namespace SalesWebMVC.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id) 
+        public IActionResult Delete(int id)
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
         }
     }
 }
